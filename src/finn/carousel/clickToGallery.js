a39c095381bdc8346939409d05e745f9b7fc5815
Michael Maurizi <mmaurizi@azavea.com>
2013-11-05 14:28:37 -0500

    Fix indentation

--- src/finn/carousel/clickToGallery.js
+++ src/finn/carousel/clickToGallery.js
@@ -3,7 +3,7 @@ FINN.carousel = FINN.carousel || {};
 
 // Add gallery url in markup:  <img data-gallery-url="">
 (function (C, $) {
-	"use strict";
+    "use strict";
 
     function sendToGallery(e) {
         var targetUrl = $(e.target).data("gallery-url");
--- src/finn/carousel/clickableElementNavigation.js
+++ src/finn/carousel/clickableElementNavigation.js
@@ -1,7 +1,7 @@
 /*! carousel-js - 2013-06-14. Copyright (c) 2013 FINN.no AS - http://finn.no/; Licensed MIT */
 (function (C, $) {
     "use strict";
-    
+
     C.setupClickableElementNavigation = function (controller, root, element, selector, getElementIndexOverride) {
         var selected = selector || "selectedElement";
 
