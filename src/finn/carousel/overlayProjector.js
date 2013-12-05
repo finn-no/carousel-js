5f1b2faad9aaa479414ad4d73b348f79c936dffe
Martin Solli <msolli@gmail.com>
2013-12-05 15:43:35 +0100

    Don't show() in buildCarousel
    Show stuff is the controller's responsibility.
    
    NOTE: This is a breaking change if you're not calling
    `controller.start()` or `controller.show()` somewhere in your
    initializing code.
    
    This should fix finn-no/carousel-js#20.

--- src/finn/carousel/overlayProjector.js
+++ src/finn/carousel/overlayProjector.js
@@ -1,7 +1,7 @@
 /*! carousel-js - 2013-06-14. Copyright (c) 2013 FINN.no AS - http://finn.no/; Licensed MIT */
 (function (C) {
     "use strict";
-    
+
     C.overlayProjector = {
         create: function (controller, data, view) {
             return FINN.compose(this, {
@@ -14,7 +14,6 @@
         buildCarousel: function () {
             var self = this;
             this.controller.on("show", function () { return self.show.apply(self, arguments); });
-            this.show(0);
             return this.view;
         },
 
